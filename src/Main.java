745b057f46158d8357efbc1f86b32166d17c897a
artur <mauboroda.arturtu@gmail.com>
2024-12-17 00:41:19 +0200

    First prog(test  push)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello World");
 
       Scanner scan = new Scanner(System.in);
        System.out.print("Enter first number: ");
