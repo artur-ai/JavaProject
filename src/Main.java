0f632c84b0042f9c2c755d2ffc8da615c2e70da6
artur <mauboroda.arturtu@gmail.com>
2024-12-25 15:23:27 +0200

    did four tasks(1-4)

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,81 @@
-//import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-       // System.out.println("Hello World");
-
-     /* Scanner scan = new Scanner(System.in);
-       System.out.print("Enter first number: ");
-       int num1 = scan.nextInt();
-
-        System.out.print("Enter second number: ");
-        int num2 = scan.nextInt();
-
-        int res = num1+num2;
-
-        System.out.print("Res1: " + res);*/
-
-        /*int[] nums = new int[4];
-        nums[0] = 1;
-        nums[1] = 2;
-        nums[2] = 3;
-        nums[3] = 4;
-        System.out.printf(String.valueOf(nums[0]));*/
+    public static void main( String[] args) {
+        // 1.  приймає масив чарів, виводить його на екран
+        char[] array = {'a', '#', '!'};
+        CharArray(array);
 
 
-        char[] sym = new char [3];
-        sym[0] = 'A';
-        sym[1] = 'C';
-        sym[2] = 'E';
-        System.out.println(sym[0]);
-        System.out.println (sym[1]);
-        System.out.println(sym[2]);
+
+        //2. приймає масив інтів, повертає масив чарів, кожен символ у позиції масиву
+        // відповідає коду символу передається інта
+        int [] array1 = {1, 5, 6, 10};
+        char[] charArray = ToCharArray(array1);
+        for (char symbol : charArray) {
+            System.out.println(symbol);
+        }
+
+
+
+        //3. приминає 2 інта, а і б, повертає більше їх цих 2х чисел
+        int a = 4;
+        int b = 8;
+        int max = a;
+        if (b>max) {
+            max = b;
+        }
+        System.out.println("Максимальне значення: " + max);
+
+
+        //4. приймає 3 інти, повертає більшу з них
+        int c = 218312;
+        int d = 321321;
+        int e = 42389;
+        int Max = c;
+        if (d>Max) {
+            Max= d;
+        }
+        if (e>Max) {
+            Max = e;
+        }
+        System.out.println("Більше з цих цифр: " + Max);
+
+
+
+
+
 
     }
 
 
-}
+    // 1. приймає масив чарів, виводить його на екран
+    public static void CharArray (char[] array) {
+        System.out.println(array);
+    }
+
+
+    /* 2.приймає масив інтів, повертає масив чарів, кожен символ у позиції масиву
+    відповідає коду символу передається інта
+    */
+
+    public static char[] ToCharArray (int[] array1){
+        System.out.println("Hello");
+        char[] charArray = {'b'};
+        return charArray;
+    }
+
+
+    //3. приминає 2 інта, а і б, повертає більше їх цих 2х чисел
+    public static int maxValue (int a, int b) {
+        return maxValue(a, b);
+    }
+
+
+    //4.приймає 3 інти, повертає більшу з них
+    public static int maxValue (int c, int e, int d){
+        return maxValue(c, d, e);
+
+    }
+
+    }
+
+
